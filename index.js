dd665d54926c8dbf2e547ccddd3e3779eaf90a96
ritikpy <ritikvarshney2014@gmail.com>
2020-05-01 17:56:47 +0530

    major commit

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const {
   Suggestions,
   Carousel,
   Image,
+  Table,
 } = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
@@ -29,6 +30,50 @@ const functions = require('firebase-functions');
 
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
+const rp = require('request-promise');
+
+var data = [
+  {
+    "status": 1,
+      "_id": "5cab9961ac295a0f3f0d9941",
+      "title": "Hands-on NodeJS Codelab",
+      "image": "",
+      "slug": "hands-on-nodejs-codelab",
+      "description": "",
+      "venue": "CSE Lab 2",
+      "startDate": "2019-02-20T05:00:00.000Z",
+      "endDate": "2019-02-28T07:00:00.000Z",
+      "startTime": "05:00 PM",
+      "__v": 0
+  },
+  {
+    "status": 1,
+    "_id": "5cab9961ac295a0f3f0d9942",
+    "title": "Hands-on Android Codelab",
+    "image": "",
+    "slug": "hands-on-android-codelab",
+    "description": "",
+    "venue": "CSE Lab 2",
+    "startDate": "2019-03-07T05:00:00.000Z",
+    "endDate": "2019-03-12T07:00:00.000Z",
+    "startTime": "05:00 PM",
+    "__v": 0
+  },
+  {
+    "status": 1,
+    "_id": "5d72718e7c213e60b8fa03cf",
+    "title": "Google Cloud Study Jam",
+    "image": "",
+    "slug": "google-cloud-study-jam",
+    "description": "",
+    "venue": "CSE Lab-2, E-Block",
+    "startDate": "2019-08-19T15:00:00.000Z",
+    "endDate": "2019-08-19T17:00:00.000Z",
+    "startTime": "03:00 PM",
+    "__v": 0
+  }
+]
+
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
@@ -62,25 +107,63 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
   }
 });
 
-// Handle the Dialogflow intent named 'favorite color'.
-// The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
-  const luckyNumber = color.length;
-  const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
-  if (conv.user.storage.userName) {
-    // If we collected user name previously, address them by name and use SSML
-    // to embed an audio snippet in the response.
-    conv.ask(`<speak>${conv.user.storage.userName}, your lucky number is ` +
-      `${luckyNumber}.<audio src="${audioSound}"></audio> ` +
-      `Would you like to hear some fake colors?</speak>`);
-    conv.ask(new Suggestions('Yes', 'No'));
-  } else {
-    conv.ask(`<speak>Your lucky number is ${luckyNumber}.` +
-      `<audio src="${audioSound}"></audio> ` +
-      `Would you like to hear some fake colors?</speak>`);
-    conv.ask(new Suggestions('Yes', 'No'));
-  }
-});
+app.intent('events', (conv) => {
+  conv.ask(new Suggestions('Projects', 'Achievement'));
+  let text, description;
+  let rows = [];
+  const y = data.length;
+    // str = str + `Title: ` + data[i].title + '\n' + `Description: ` + data[i].slug + `\n`;
+    for(var i=0;i<y;i++)
+    {
+      text = data[i].title;
+      description = data[i].slug;
+      rows.push([text, description]);
+    }
+    conv.ask('Here is the list of all events');
+    conv.ask(new Table({
+      dividers: true,
+      columns: ['text','description'],
+      rows: rows,
+      // title: data[0].title,
+      // text: data[0].slug
+    }));
+  // conv.ask(str);
+
+  // const options = {
+  //   method:'GET',
+  //   url:'https://dsckiet-backend.herokuapp.com/api/v1_old/events',
+  //   headers: {
+  //     'User-Agent': 'Request-Promise'
+  //   },
+  //   json:true
+  // }
+  //   rp(options)
+  //   .then(function(bodyParser){
+  //     var y = bodyParser.pastevents.length;
+  //     console.log(y)
+  //     var data = [];
+  //     for(var i=0;i<y;i++)
+  //     {
+  //       data.push(bodyParser.pastevents[i].title);
+  //     }
+  //     var slugarr = [];
+  //     for(var i=0;i<y;i++)
+  //     {
+  //       slugarr.push(bodyParser.pastevents[i].slug);
+  //     }
+  //     var str = 'here';
+  //     for(i=0;i<y;i++)
+  //     {
+  //       var z=i+1;
+  //       str = str + `Details of events`+ z + `Title: ` + data[i]+`\n`+ `Description: `+ slugarr[i]+`.`;
+  //     }
+  //     return conv.ask(`${str}`);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     conv.ask(`errors`);
+  //   })
+})
 
 
 // Handle the Dialogflow NO_INPUT intent.
